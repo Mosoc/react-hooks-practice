29d09bf58927a3f349e9bd21136c4907bc74a344
Bruno Anderson Chen <mosoc@colle.co>
2019-10-14 15:06:45 +0900

    Simply implement the handler functions

--- src/UseState/CountryAndCity.js
+++ src/UseState/CountryAndCity.js
@@ -5,11 +5,34 @@ function CountyAndCity() {
     city: '',
     country: ''
   });
+
+  function handleCityChange(event) {
+    setState({
+      city: event.target.value
+    });
+  }
+
+  function handleCountryChange(event) {
+    setState({
+      country: event.target.value
+    });
+  }
+
   return (
     <section>
       <form>
-        <input type="text" value={state.city} placeholder="City" />
-        <input type="text" value={state.country} placeholder="Country" />
+        <input
+          type="text"
+          placeholder="City"
+          value={state.city}
+          onChange={handleCityChange}
+        />
+        <input
+          type="text"
+          placeholder="Country"
+          value={state.country}
+          onChange={handleCountryChange}
+        />
         <div>You live in {`${state.city}, ${state.country}`}</div>
       </form>
     </section>
