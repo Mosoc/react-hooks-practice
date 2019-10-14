7b19f8e0d23b5b635f0672d142d4808a4643cec5
Bruno Anderson Chen <mosoc@colle.co>
2019-10-14 13:13:01 +0900

    Use predefined function

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import React, {useState} from 'react';
 function App(){
   const [activated, setActivated] = useState(false);
   const buttonText = activated ? 'Active': 'Inactive';
+  function handleClick() {
+    setActivated(!activated)
+  }
   return (
     <React.Fragment>
+      <label>Use an arrow function in props</label>
       <button onClick={()=>{setActivated(!activated)}} >
+        {buttonText}
+      </button>
+      <label>Use predefined function</label>
+      <button onClick={handleClick} >
         {buttonText}
       </button>
     </React.Fragment>
